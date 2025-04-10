6eefba72adfe530a0dee87998d0bf9f19bf3d2b1
Dulani-Viyathma <dulanii2002@gmail.com>
2025-04-10 10:14:30 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RecipeDetailsPage from './pages/RecipeDetailsPage';
+
 function App() {
   return (
-    <div>
-      <h1>Flavor Exchange App</h1>
-      <p>Welcome to the recipe sharing platform!</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/recipe/:id" element={<RecipeDetailsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
