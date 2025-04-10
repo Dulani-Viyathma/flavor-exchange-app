504dabf68d779bddba3f2f4dec00db9086847f39
Dulani-Viyathma <dulanii2002@gmail.com>
2025-04-10 10:08:00 +0530

    added navigation to the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function HomePage() {
-    return <h2>Home Page - List of Recipes will show here</h2>;
-  }
-  
-  export default HomePage;
-  
+  return (
+    <div>
+      <h2>Home Page - List of Recipes will show here</h2>
+      <nav>
+        <Link to="/login">Go to Login Page</Link><br />
+        <Link to="/recipe/1">See Recipe 1</Link>
+      </nav>
+    </div>
+  );
+}
+
+export default HomePage;
